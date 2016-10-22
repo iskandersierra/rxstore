d55cec9ff15b0409e2031380f8e82b3f19e3077a
iskander.sierra@gmail.com <iskander.sierra@gmail.com>
2016-10-22 10:48:51 +0200

    fix(logEffects): Removed log effects helpers

--- src/createEffects.ts
+++ src/createEffects.ts
@@ -9,21 +9,6 @@ import {
 
 const scheduler = queue;
 
-export const logUpdatesEffect =
-  (captioner: (update: StateUpdate<any>, store: Store<any>) => any) =>
-    (logger: (message?: any, ...parameters: any[]) => void) =>
-      (store: Store<any>) => {
-        const effect = (up: StateUpdate<any>) => logger(captioner(up, store), up);
-        return store.update$.do(effect);
-      };
-
-export const logUpdatesByActionTypeEffect =
-  logUpdatesEffect((up, store) => {
-    let caption = (store as any).caption;
-    if (caption) { caption += ": "; } else { caption = ""; }
-    return caption + "ON " + up.action.type;
-  });
-
 export const createEffects =
   (dispatch: Dispatcher, ...effects: Effect[]): void =>
     effects.forEach(e => e.subscribeOn(scheduler).subscribe(dispatch));
